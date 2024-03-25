2e3f386cbd385de939b74894ed7d614242ff078a
Shalom-uwamahoro <shalomuwamahoro@gmail.com>
2024-03-25 08:26:20 +0300

    added answers

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,45 @@
+
+fun main() {
+    stringFun("Barnie bakes brown bagels and buns")
+
+    var arrayInt= arrayOf(23, 56, 78)
+    println(arrayInt.sum())
+    println(arrayInt.count())
+    println(arrayInt.average())
+
+
+   println(volumeFun(14.56))
+
+   println(isPalindrome("madam"))
+    println(isPalindrome("shalom"))
+
+
+
+}
+
+fun stringFun(word: String) {
+    println(word.replace("b", ""))
+}
+
+fun arrayInt(num1: Int, num2: Int, num3: Int) {
+    var sum = num1 + num2 + num3
+    var average= sum/3
+
+}
+fun volumeFun(r: Double): Double{
+    var volume= 4/3 *3.14159* r
+    return volume
+}
+
+
+fun isPalindrome(word: String) {
+    var palindrome = word.reversed()
+    if (word == palindrome) {
+        println("true")
+    } else {
+        println(false)
+    }
+}
+
+
+
